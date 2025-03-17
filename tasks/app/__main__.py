005b309e865677a38eba2292b165504d585c5f1b
endrit101 <64744161+endrit101@users.noreply.github.com>
2025-03-17 14:43:15 +0100

    revert this change

--- tasks/app/__main__.py
+++ tasks/app/__main__.py
@@ -4,6 +4,4 @@ import uvicorn
 
 source_directory = os.path.dirname(os.path.realpath(__file__))
 os.environ["UVICORN_RELOAD_DIRS"] = source_directory
-
-if __name__ == "__main__":
-    uvicorn.run(host="0.0.0.0", app="fns:app_factory")
+uvicorn.main()
