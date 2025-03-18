921b45e6ebe6a70671c52bb061ecc67d87af8696
endrit101 <endrit.toplica@gmail.com>
2025-03-18 11:22:11 +0100

    change the conflict

--- tasks/app/__main__.py
+++ tasks/app/__main__.py
@@ -4,10 +4,4 @@ import uvicorn
 
 source_directory = os.path.dirname(os.path.realpath(__file__))
 os.environ["UVICORN_RELOAD_DIRS"] = source_directory
-<<<<<<< HEAD
 uvicorn.main()
-=======
-
-if __name__ == "__main__":
-    uvicorn.main()
->>>>>>> 3e4e68f (applying comments)
