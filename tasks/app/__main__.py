9eb38faf4d81fda795a342307860d9e4db6211dd
ChrisJackHowe <chowe@consynsys.com>
2026-04-16 11:47:55 -0700

    initial draft (tested)

--- tasks/app/__main__.py
+++
@@ -1,7 +0,0 @@
-import os
-import uvicorn
-
-
-source_directory = os.path.dirname(os.path.realpath(__file__))
-os.environ["UVICORN_RELOAD_DIRS"] = source_directory
-uvicorn.main()
--- views/system-run-log-and-file-handling/src/shadow/PortalContainerContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const PortalContainerContext = createContext<HTMLElement | null>(null);
-
-export const usePortalContainer = () => {
-    return useContext(PortalContainerContext);
-};
