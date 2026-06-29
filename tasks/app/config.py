dc869a5d104489020a0fbcba72c82a12bfcc53e1
endrit101 <endrit.toplica@gmail.com>
2026-06-29 15:19:25 +0200

    test

--- tasks/app/config.py
+++ tasks/app/config.py
@@ -5,7 +5,7 @@ import functools
 class Config(pydantic.BaseSettings):
     example_config_variable: str = "default value"
 
-
+# test if auto commit worked
 @functools.lru_cache
 def get_config() -> Config:
     return Config()
